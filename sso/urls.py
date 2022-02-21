6803eab98193fd27d44758a3f3eaf0aaf5113020
Dan <dan@Dans-Mac-mini.local>
2022-02-20 19:16:13 -0800

    trying to fix bad requests

--- sso/urls.py
+++ sso/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    path('/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('auth', views.auth, name='auth'),
     path('auth/callback', views.auth_callback, name='auth_callback'),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/sso/static/images/favicon')),
 ]
