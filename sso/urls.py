4294f77d6c21c9a89ac5d29f1bace55b97b12bc8
Dan <dan@Dans-Mac-mini.local>
2022-02-20 19:05:13 -0800

    update urls.py

--- sso/urls.py
+++ sso/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('/', views.login, name='login'),
     path('auth', views.auth, name='auth'),
     path('auth/callback', views.auth_callback, name='auth_callback'),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/sso/static/images/favicon.ico')),
-    
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/sso/static/images/favicon')),
+
 ]
