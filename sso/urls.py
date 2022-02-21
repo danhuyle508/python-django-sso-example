138972e5c00c3d3b29e9913e1a9c8a14d1a8e05f
danhuyle508 <38339026+danhuyle508@users.noreply.github.com>
2022-02-20 18:20:03 -0800

    Update urls.py

--- sso/urls.py
+++ sso/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('auth', views.auth, name='auth'),
     path('auth/callback', views.auth_callback, name='auth_callback'),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/sso/static/images/favicon.ico')),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/sso/static/images/favicon')),
     
 ]
